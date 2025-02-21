f3f459348416545b2e2507056a561158fb500860
Mr.Elijah <beztalannyj.ilia@gmail.com>
2025-02-21 02:39:37 -0800

    react first homework

--- src/main.jsx
+++ src/main.jsx
@@ -4,8 +4,4 @@ import "modern-normalize";
 import "./index.css";
 import App from "./components/App/App";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
-);
+createRoot(document.getElementById("root")).render(<App />);
